b9fcb01a0a1fb90400279c2dbee6ae9401add8c2
Mark <mgarnick@techtonicgroup.com>
2020-10-14 11:11:05 -0600

    added feather icons and globally add base components

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,12 +2,14 @@
   <div>
     <h1>Event Listing</h1>
     <EventCard />
+    <BaseIcon name="activity" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue';
 export default {
+  name: 'EventList',
   components: { EventCard },
 };
 </script>
