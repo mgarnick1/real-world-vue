0731aba33246537282cd37997e2e3102f7fd8488
Mark <mgarnick@techtonicgroup.com>
2020-10-14 09:27:05 -0600

    fix routing play around with alias and redirect and use named routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,36 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import EventList from '../views/EventList.vue';
+import EventShow from '../views/EventShow.vue';
+import EventCreate from '../views/EventCreate.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'event-list',
+    component: EventList,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/event',
+    name: 'event-show',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: EventShow,
+    // alias: '/about',
   },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate,
+  },
+  // {
+  //   path: '/about',
+  //   redirect: { name: 'about' },
+  // },
 ];
 
 const router = new VueRouter({
